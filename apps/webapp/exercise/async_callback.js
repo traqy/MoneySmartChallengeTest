c7e31d28083f51e2ec62776b67fa7f20324a8000
traqy <bernard.t@mig.me>
2016-08-20 00:51:51 +0800

    added sample webapp js files.

---
+++ apps/webapp/exercise/async_callback.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+fs.readFile('sample.txt',function(err, data){
+    if (err){
+        console.log(err.stack);
+        return ;
+    }
+    console.log(data.toString());
+    
+})
+
+console.log("Program Ended")
