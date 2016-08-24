358f175413ee37ae30c458eab3861ae1f1d745f5
traqy <bernard.t@mig.me>
2016-08-25 00:40:55 +0800

    Added css and tweak docker file for web-ci

---
+++ apps/web-codeigniter/booking/application/views/booking_about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Tennis Court Schedule Pick A Date</title>  
+  <?php echo link_tag('assets/css/booking-webapp-table.css'); ?>
+  <?php echo link_tag('assets/css/menu.css'); ?> 
+  <?php echo link_tag('assets/css/text-input.css'); ?>  
+</head>
+
+<body>
+
+<div>
+<ul>
+  <li><a href="about">About</a></li>
+  <li><a href="login">Login</a></li>
+</div>
+
+<div>
+<label>This is just a technical test exercise.</label>
+<label>Author: btraquena@gmail.com</label>
+</div>
+
+</body>
+</html>
